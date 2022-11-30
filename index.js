867132e139466943aaeb62490b406b0117395764
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-30 18:15:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName='bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'Mary';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    
+    }
+    
+// console.log(upperCaseCustomerName());
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not mary';
+}
+
+   
